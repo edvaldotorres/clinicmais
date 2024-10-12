f6dfb2f1d0e8c72ed9c6a93121f7d0d1a58d4d2f
Edvaldo Torres <57301962+edvaldotorres@users.noreply.github.com>
2024-10-12 10:58:29 -0300

    feature: paginate component

--- admin/app/Http/Controllers/PostController.php
+++ admin/app/Http/Controllers/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(10);
 
         return Inertia::render('Post/Index', [
             'posts' => $posts
--- admin/resources/js/Pages/Post/Index.vue
+++ admin/resources/js/Pages/Post/Index.vue
@@ -10,19 +10,38 @@ import {
     TableHeader,
     TableRow,
 } from '@/shadcn/ui/table';
-import { Button } from '@/shadcn/ui/button'; // Importando o botão do ShadCN Vue
-import { usePage } from '@inertiajs/vue3'; // Importando para acessar os dados enviados do backend
+import { Button } from '@/shadcn/ui/button';
+import {
+    Pagination,
+    PaginationEllipsis,
+    PaginationFirst,
+    PaginationLast,
+    PaginationList,
+    PaginationListItem,
+    PaginationNext,
+    PaginationPrev,
+} from '@/shadcn/ui/pagination';
+
+import { ref } from 'vue'; // Para manipular o estado de paginação
+import { usePage } from '@inertiajs/vue3';
 
 // Acessando os dados enviados do backend (Inertia)
 const { posts } = usePage().props;
 
-// Função para deletar um post (exemplo)
-const deletePost = (id) => {
-    if (confirm('Tem certeza que deseja deletar este post?')) {
-        router.delete(route('posts.destroy', id)); // Assumindo que você tenha uma rota 'posts.destroy' configurada
-    }
+// Acessando os posts e os dados de paginação
+const currentPage = ref(posts.current_page); // Página atual
+const totalPages = ref(posts.last_page); // Total de páginas
+
+// Função para trocar de página
+const changePage = (page) => {
+    if (page < 1 || page > totalPages.value) return; // Verificar limites de página
+    router.get(route('posts.index', { page }), {
+        preserveState: true, // Manter o estado da página
+        preserveScroll: true, // Manter a posição de rolagem
+    });
 };
 </script>
+
 
 <template>
 
@@ -37,14 +56,14 @@ const deletePost = (id) => {
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                     <header>
-                        <h2 class="text-lg font-medium text-gray-900">Post Information</h2>
+                        <h2 class="text-lg font-medium text-gray-900">Informações de Post</h2>
                     </header>
 
-                    <!-- Alinhamento do botão à direita e responsivo -->
+                    <!-- Botão de Cadastro -->
                     <div class="flex justify-end mb-8">
                         <Link :href="route('posts.create')">
                         <Button variant="default" class="px-6 py-3 text-lg">
-                            Ir para Cadastro
+                            Criar Novo Post
                         </Button>
                         </Link>
                     </div>
@@ -52,34 +71,69 @@ const deletePost = (id) => {
                     <!-- Tabela de posts -->
                     <div class="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900">
-                            <Table class="w-full border-collapse">
-                                <TableCaption class="text-lg font-semibold mb-4">A list of your recent posts
-                                </TableCaption>
-                                <TableHeader>
-                                    <TableRow class="bg-gray-100">
-                                        <TableHead class="p-4 text-left font-bold text-gray-700 border-b">Title
-                                        </TableHead>
-                                        <TableHead class="p-4 text-left font-bold text-gray-700 border-b">Content
-                                        </TableHead>
-                                        <TableHead class="p-4 text-left font-bold text-gray-700 border-b">Options
-                                        </TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    <!-- Exibição dos posts dinamicamente -->
-                                    <TableRow v-for="(post, index) in posts" :key="index"
-                                        class="hover:bg-gray-50 transition-colors border-b last:border-none">
-                                        <TableCell class="p-4 font-medium text-gray-900">{{ post.title }}</TableCell>
-                                        <TableCell class="p-4 text-gray-700">{{ post.content }}</TableCell>
-                                        <TableCell class="p-4 flex space-x-2">
-                                            <Link :href="route('posts.edit', post.id)">
-                                            <Button variant="outline" class="text-blue-600">Editar</Button>
-                                            </Link>
-                                            <Button variant="destructive" @click="deletePost(post.id)">Deletar</Button>
-                                        </TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
+                            <div class="overflow-x-auto">
+                                <Table class="w-full border-collapse">
+                                    <TableCaption class="text-lg font-semibold mb-4">Lista de posts recentes
+                                    </TableCaption>
+                                    <TableHeader>
+                                        <TableRow class="bg-gray-100">
+                                            <TableHead class="p-4 text-left font-bold text-gray-700 border-b">Título
+                                            </TableHead>
+                                            <TableHead class="p-4 text-left font-bold text-gray-700 border-b">Conteúdo
+                                            </TableHead>
+                                            <TableHead class="p-4 text-left font-bold text-gray-700 border-b">Opções
+                                            </TableHead>
+                                        </TableRow>
+                                    </TableHeader>
+                                    <TableBody>
+                                        <!-- Exibição dos posts dinamicamente -->
+                                        <TableRow v-for="(post, index) in posts.data" :key="index"
+                                            class="hover:bg-gray-50 transition-colors border-b last:border-none">
+                                            <TableCell class="p-4 font-medium text-gray-900">{{ post.title }}
+                                            </TableCell>
+                                            <TableCell class="p-4 text-gray-700">{{ post.content }}</TableCell>
+                                            <TableCell class="p-4 flex space-x-2">
+                                                <Link :href="route('posts.edit', post.id)">
+                                                <Button variant="outline" class="text-blue-600">Editar</Button>
+                                                </Link>
+                                                <Button variant="destructive"
+                                                    @click="deletePost(post.id)">Deletar</Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+
+                                <!-- Componente de Paginação -->
+                                <Pagination v-slot="{ page }" :total="totalPages * 10" :sibling-count="1" show-edges
+                                    :default-page="currentPage">
+                                    <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+                                        <!-- Ir para primeira página -->
+                                        <PaginationFirst @click="changePage(1)" />
+
+                                        <!-- Ir para página anterior -->
+                                        <PaginationPrev @click="changePage(currentPage - 1)" />
+
+                                        <template v-for="(item, index) in items" :key="index">
+                                            <PaginationListItem v-if="item.type === 'page'" :value="item.value"
+                                                as-child>
+                                                <Button class="w-10 h-10 p-0"
+                                                    :variant="item.value === page ? 'default' : 'outline'"
+                                                    @click="changePage(item.value)">
+                                                    {{ item.value }}
+                                                </Button>
+                                            </PaginationListItem>
+                                            <PaginationEllipsis v-else :key="item.type" :index="index" />
+                                        </template>
+
+                                        <!-- Ir para próxima página -->
+                                        <PaginationNext @click="changePage(currentPage + 1)" />
+
+                                        <!-- Ir para última página -->
+                                        <PaginationLast @click="changePage(totalPages)" />
+                                    </PaginationList>
+                                </Pagination>
+
+                            </div>
                         </div>
                     </div>
                 </div>
