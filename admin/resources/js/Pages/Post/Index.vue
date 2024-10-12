86303b5b7ac21c6af1dd239d1ec8def44063f44d
Edvaldo Torres <57301962+edvaldotorres@users.noreply.github.com>
2024-10-11 23:54:11 -0300

    feature: add table component

--- admin/resources/js/Pages/Post/Index.vue
+++ admin/resources/js/Pages/Post/Index.vue
@@ -1,6 +1,15 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/shadcn/ui/table';
 </script>
 
 <template>
@@ -15,7 +24,30 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <!-- Tabela de faturas recentes -->
+                    <div class="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            <Table>
+                                <TableCaption>A list of your recent invoices.</TableCaption>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead class="w-[100px]">Invoice</TableHead>
+                                        <TableHead>Status</TableHead>
+                                        <TableHead>Method</TableHead>
+                                        <TableHead class="text-right">Amount</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell class="font-medium">INV001</TableCell>
+                                        <TableCell>Paid</TableCell>
+                                        <TableCell>Credit Card</TableCell>
+                                        <TableCell class="text-right">$250.00</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
