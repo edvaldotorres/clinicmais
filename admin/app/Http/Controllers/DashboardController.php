f7115e48aa027e15c02499e05aad250057e55c75
Edvaldo Torres <57301962+edvaldotorres@users.noreply.github.com>
2024-10-12 21:41:56 -0300

    [...]

--- admin/app/Http/Controllers/DashboardController.php
+++ admin/app/Http/Controllers/DashboardController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\PostRequest;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
-use App\Traits\UploadFile;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
-use Illuminate\Support\Str;
 
 class DashboardController extends Controller
 {
@@ -18,12 +14,10 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        // Obtenção dos totais de posts, comentários e usuários
         $totalPosts = Post::count();
         $totalComments = Comment::count();
         $totalUsers = User::count();
 
-        // Retorna para o frontend usando Inertia com os dados
         return Inertia::render('Dashboard', [
             'totalPosts' => $totalPosts,
             'totalComments' => $totalComments,
